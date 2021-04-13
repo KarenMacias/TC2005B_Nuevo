567da8bb9eac834544cf06f565a04135394511cb
KarenMacias <79269657+KarenMacias@users.noreply.github.com>
2021-04-12 19:05:16 -0500

    primary key de user

--- sitioweb/videojuego/models.py
+++ sitioweb/videojuego/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    id = models.IntegerField(primary_key = True)
     names = models.CharField(max_length=60)
     lastname1 = models.CharField(max_length=30)
     lastname2 = models.CharField(max_length=30)
